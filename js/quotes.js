00aa95d8c7672f832948dbb88d0ce578fa3cba70
Alex Piña <alexpina224@gmail.com>
2025-03-20 19:46:06 -0700

    visible timer, darker colors, hover hold

---
+++ js/quotes.js
@@ -0,0 +1,105 @@
+const quotes = [
+    {
+      quote: "Hello There.",
+      source: "Obi Wan Kenobi", year: "19 BBY", location:"Utapau", citation: "Star Wars:Episode 3 Revenge of the Sith", gif: "https://media.giphy.com/media/8JTFsZmnTR1Rs1JFVP/giphy.gif"
+    },
+    {
+      quote: "Is it possible to learn this power?",
+      source: "Anakin Skywalker", year: "19 BBY", location:"Coruscant", citation: "Star Wars:Episode 3 Revenge of the Sith"
+    },
+    {
+      quote: "Did you ever hear the Tragedy of Darth Plagueis the wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It's ironic he could save others from death, but not himself.",
+      source: "Sheev Palpatine", year: "19 BBY", location:"Coruscant", citation: "Star Wars:Episode 3 Revenge of the Sith"
+    },
+    {
+      quote: "There’s Always A Bigger Fish.",
+      source: "Qui-Gon Jin", year: "32 BBY", location:"Naboo", citation: "Star Wars:Episode 1 The Phantom Menace"
+    },
+    {
+      quote: "Fear is the path to the Dark Side. Fear leads to anger. Anger leads to hate. Hate, leads to suffering.",
+      source: "Master Yoda", year: "32 BBY", location:"Coruscant", citation: "Star Wars:Episode 1 The Phantom Menace"
+    },
+    {
+      quote: "I don't like sand. It's coarse and rough and irritating and it gets everywhere. Not like here. Here everything is soft and smooth.",
+      source: "Anakin Skywalker", year: "22 BBY", location:"Naboo", citation: "Star Wars:Episode 2 Attack of the Clones"
+    },
+    {
+      quote: "I killed them, I killed them all. There dead. Every single one of them. But not just the men, but the women, and the children too. There like animals! And I slaughtered theme like animals! I hate them!",
+      source: "Anakin Skywalker", year: "22 BBY", location:"Tatooine", citation: "Star Wars:Episode 2 Attack of the Clones"
+    },
+    {
+      quote: "I'm just a simple man, trying to make my way in the universe.",
+      source: "Jango Fett", year: "22 BBY", location:"Kamino", citation: "Star Wars:Episode 2 Attack of the Clones"
+    },
+    {
+        quote: "I find your lack of faith disturbing.",
+        source: "Darth Vader",
+        year: "0 BBY",
+        location: "Death Star",
+        citation: "Star Wars:Episode 4 A New Hope"
+      },
+      {
+        quote: "Help me, Obi-Wan Kenobi. You're my only hope.",
+        source: "Princess Leia",
+        year: "0 BBY",
+        location: "Tantive IV",
+        citation: "Star Wars:Episode 4 A New Hope"
+      },
+      {
+        quote: "Never tell me the odds.",
+        source: "Han Solo",
+        year: "3 ABY",
+        location: "Hoth",
+        citation: "Star Wars:Episode 5 The Empire Strikes Back"
+      },
+      {
+        quote: "Do or do not. There is no try.",
+        source: "Master Yoda",
+        year: "3 ABY",
+        location: "Dagobah",
+        citation: "Star Wars:Episode 5 The Empire Strikes Back"
+      },
+      {
+        quote: "No. I am your father.",
+        source: "Darth Vader",
+        year: "3 ABY",
+        location: "Bespin",
+        citation: "Star Wars:Episode 5 The Empire Strikes Back"
+      },
+      {
+        quote: "It's a trap!",
+        source: "Admiral Ackbar",
+        year: "4 ABY",
+        location: "Battle of Endor",
+        citation: "Star Wars:Episode 6 Return of the Jedi"
+      },
+      {
+        quote: "That's not how the Force works!",
+        source: "Han Solo",
+        year: "34 ABY",
+        location: "Starkiller Base",
+        citation: "Star Wars:Episode 7 The Force Awakens"
+      },
+      {
+        quote: "Let the past die. Kill it, if you have to.",
+        source: "Kylo Ren",
+        year: "34 ABY",
+        location: "Supremacy",
+        citation: "Star Wars:Episode 8 The Last Jedi"
+      },
+      {
+        quote: "Somehow Palpatine returned.",
+        source: "Poe Dameron",
+        year: "35 ABY",
+        location: "Resistance Base",
+        citation: "Star Wars:Episode 9 The Rise of Skywalker"
+      },
+      {
+        quote: "I'm one with the Force. The Force is with me.",
+        source: "Chirrut Îmwe",
+        year: "0 BBY",
+        location: "Jedha",
+        citation: "Rogue One: A Star Wars Story"
+      }
+  ]
+  export default quotes;
--- js/script.js
+++ js/script.js
@@ -1,104 +1,184 @@
 /******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+ * Random Quote Generator - Enhanced
 ******************************************/
 
-// For assistance:
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+import quotes from "./quotes.js";
+
+/* 
+  We'll store the last displayed quote text & source
+  so we can tweet them. 
+*/
+let currentQuoteText = "";
+let currentQuoteSource = "";
+
+/* Timer variables */
+let previousIndex = 0;
+let randomIndex;
+let timeLeft = 5;
+let countdownIntervalId;
+let quoteTimeoutId;
 
 /***
- * `quotes` array - holds all the quote objects
-***/
-const quotes = [
-  {
-    quote: "Hello There.",
-    source: "Obi Wan Kenobi", year: "19 BBY", location:"Utapau", citation: "Star Wars:Episode 3 Revenge of the Sith"
-  },
-  {
-    quote: "Is it possible to learn this power?",
-    source: "Anakin Skywalker", year: "19 BBY", location:"Coruscant", citation: "Star Wars:Episode 3 Revenge of the Sith"
-  },
-  {
-    quote: "Did you ever hear the Tragedy of Darth Plagueis the wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It's ironic he could save others from death, but not himself.",
-    source: "Sheev Palpatine", year: "19 BBY", location:"Coruscant", citation: "Star Wars:Episode 3 Revenge of the Sith"
-  },
-  {
-    quote: "There’s Always A Bigger Fish.",
-    source: "Qui-Gon Jin", year: "32 BBY", location:"Naboo", citation: "Star Wars:Episode 1 The Phantom Menace"
-  },
-  {
-    quote: "Fear is the path to the Dark Side. Fear leads to anger. Anger leads to hate. Hate, leads to suffering.",
-    source: "Master Yoda", year: "32 BBY", location:"Coruscant", citation: "Star Wars:Episode 1 The Phantom Menace"
-  },
-  {
-    quote: "I don't like sand. It's coarse and rough and irritating and it gets everywhere. Not like here. Here everything is soft and smooth.",
-    source: "Anakin Skywalker", year: "22 BBY", location:"Naboo", citation: "Star Wars:Episode 2 Attack of the Clones"
-  },
-  {
-    quote: "I killed them, I killed them all. There dead. Every single one of them. But not just the men, but the women, and the children too. There like animals! And I slaughtered theme like animals! I hate them!",
-    source: "Anakin Skywalker", year: "22 BBY", location:"Tatooine", citation: "Star Wars:Episode 2 Attack of the Clones"
-  },
-  {
-    quote: "I'm just a simple man, trying to make my way in the universe.",
-    source: "Jango Fett", year: "22 BBY", location:"Kamino", citation: "Star Wars:Episode 2 Attack of the Clones"
-  }
-]
-
-var previousIndex = 0;
-var randomIndex;
-
-/***
- * `getRandomQuote` function
+ * Returns a random integer between `lower` and `upper` (inclusive)
 ***/
 function randomNumberBetween(lower, upper) {
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
-function getRandomQuote(){
-  //get a random index
-  randomIndex = randomNumberBetween(0, (quotes.length -1))
-  //prevents repeated quotes if index matches previous
-  while (randomIndex === previousIndex){
-    randomIndex = randomNumberBetween(0, (quotes.length -1))
+
+/***
+ * Returns a random quote from `quotes`, ensuring we don't repeat the last one
+***/
+function getRandomQuote() {
+  randomIndex = randomNumberBetween(0, quotes.length - 1);
+  while (randomIndex === previousIndex) {
+    randomIndex = randomNumberBetween(0, quotes.length - 1);
   }
-  //set previous index
   previousIndex = randomIndex;
   return quotes[randomIndex];
 }
 
 /***
- * `printQuote` function
+ * Update the countdown display in HTML
 ***/
-function printQuote(){
-  var ramdomQuote = getRandomQuote();
-  var htmlOutput = `<p class="quote">${ramdomQuote.quote}</p>
-                    <p class="source">${ramdomQuote.source}`;
-
-  if (ramdomQuote.citation){
-    htmlOutput += `<span class="citation">${ramdomQuote.citation}</span>`
-  }
-  if (ramdomQuote.location){
-    htmlOutput += `<span class="location">${ramdomQuote.location}</span>`
-  }
-  if (ramdomQuote.year){
-    htmlOutput += `<span class="year">${ramdomQuote.year}</span>`
-  }
-
-  htmlOutput += `</p>`
-  //update web page
-  document.getElementById('quote-box').innerHTML = htmlOutput;
-  //update background color
-  document.body.style.backgroundColor = `rgb(${randomNumberBetween(0,255)}, ${randomNumberBetween(0,255)}, ${randomNumberBetween(0,255)})`;
-  return htmlOutput;
+function updateCountdownDisplay() {
+  document.getElementById("countdown").textContent = timeLeft;
 }
 
+/***
+ * Start or restart timers:
+ *   1) 1-second interval to decrement timeLeft
+ *   2) timeLeft-second timeout to call printQuote()
+***/
+function startTimers() {
+  clearTimers();     // clear existing timers
+  updateCountdownDisplay(); // show correct time initially
 
-//set the interval to automatically refresh quotes every 5 seconds
-setInterval(printQuote,5000);
+  countdownIntervalId = setInterval(() => {
+    timeLeft--;
+    updateCountdownDisplay();
+    if (timeLeft <= 0) {
+      printQuote();
+    }
+  }, 1000);
+
+  quoteTimeoutId = setTimeout(() => {
+    printQuote();
+  }, timeLeft * 1000);
+}
 
 /***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
+ * Clear both countdown & quote timeout
+***/
+function clearTimers() {
+  clearInterval(countdownIntervalId);
+  clearTimeout(quoteTimeoutId);
+}
+
+/***
+ * Generate a random "darker" background color
+ * so white text remains readable
+***/
+function getDarkerBackgroundColor() {
+  const r = randomNumberBetween(0, 150);
+  const g = randomNumberBetween(0, 150);
+  const b = randomNumberBetween(0, 150);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+/***
+ * Print a new quote, update background, reset timers
+***/
+function printQuote() {
+  const randomQuote = getRandomQuote();
+  
+  // Build the HTML text
+  let htmlOutput = `
+    <p class="quote">${randomQuote.quote}</p>
+    <p class="source">${randomQuote.source}
+  `;
+  if (randomQuote.citation) {
+    htmlOutput += `<span class="citation">${randomQuote.citation}</span>`;
+  }
+  if (randomQuote.location) {
+    htmlOutput += `<span class="location">${randomQuote.location}</span>`;
+  }
+  if (randomQuote.year) {
+    htmlOutput += `<span class="year">${randomQuote.year}</span>`;
+  }
+  // If a gif property exists, add an <img> tag
+  if (randomQuote.gif) {
+    // Optionally wrap <img> in a container if you prefer styling
+    htmlOutput += `
+      <div class="gif-container">
+        <img src="${randomQuote.gif}" alt="Star Wars GIF" class="quote-gif">
+      </div>
+    `;
+  }
+  htmlOutput += `</p>`;
+
+  // Update the quote-box
+  document.getElementById("quote-box").innerHTML = htmlOutput;
+
+  // Save the text & source for tweeting
+  currentQuoteText = randomQuote.quote;
+  currentQuoteSource = randomQuote.source || "";
+
+  // Update the background to a darker RGB
+  document.body.style.backgroundColor = getDarkerBackgroundColor();
+
+  // Reset the countdown to 5 seconds
+  timeLeft = 5;
+  startTimers();
+}
+
+/***
+ * Freeze (pause) the countdown. Called on mouseover.
+***/
+function freezeTimer() {
+  // Stop intervals/timeouts
+  clearTimers();
+  // Add "paused" class to .container for the glow
+  document.querySelector(".container").classList.add("paused");
+}
+
+/***
+ * Resume from the point it was paused. Called on mouseleave.
+***/
+function resumeTimer() {
+  // Remove "paused" class
+  document.querySelector(".container").classList.remove("paused");
+  // Re-start the timers from the current `timeLeft`
+  startTimers();
+}
+
+/***
+ * Tweet the current quote in a new tab
+***/
+function tweetQuote() {
+  // Format: "Quote text" -- Source
+  // URL-encode the text
+  const textToTweet = encodeURIComponent(`"${currentQuoteText}" — ${currentQuoteSource}`);
+  // Compose the Twitter share link
+  const twitterURL = `https://twitter.com/intent/tweet?text=${textToTweet}`;
+  // Open in a new tab
+  window.open(twitterURL, "_blank");
+}
+
+/***
+ * Set up event listeners
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// Button to show another quote
+document.getElementById("load-quote").addEventListener("click", printQuote);
+
+// Button to tweet the quote
+document.getElementById("tweet-quote").addEventListener("click", tweetQuote);
+
+// Hover over quote box -> freeze
+document.getElementById("quote-box").addEventListener("mouseover", freezeTimer);
+document.getElementById("quote-box").addEventListener("mouseleave", resumeTimer);
+
+/***
+ * Initial setup
+***/
+printQuote();
